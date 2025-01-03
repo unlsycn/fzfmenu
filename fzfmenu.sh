c87e1942a30aafa6c0f7deedaf9083f9b84e013e
unlsycn <unlsycn@unlsycn.com>
2025-01-04 00:56:50 +0800

    refactor: inline wrapper script
    Signed-off-by: unlsycn <unlsycn@unlsycn.com>

--- fzfmenu.sh
+++ fzfmenu.sh
@@ -8,9 +8,17 @@ pipe_input=$(genRandomFifo)
 pipe_output=$(genRandomFifo)
 mkfifo ${pipe_input} ${pipe_output}
 
+wrapper_script=/tmp/fzfmenu-wrapper.sh
+echo ' 
+    pipe_input=$1
+    pipe_output=$2
+    shift 2
+    cat ${pipe_input} | fzf "$@" >${pipe_output}
+    ' >${wrapper_script}
+
 cat >>${pipe_input} <&0 &
 {
-    hyprctl clients | rg "title: ${name}$" || alacritty -T ${name} -o 'font.size=18' -e bash fzfmenu-wrapper ${pipe_input} ${pipe_output} "$@"
+    hyprctl clients | rg "title: ${name}$" || alacritty -T ${name} -o 'font.size=18' -e bash ${wrapper_script} ${pipe_input} ${pipe_output} "$@"
     hyprctl dispatch focuswindow "title:${name}$"
 } &>/dev/null &
 cat ${pipe_output}
